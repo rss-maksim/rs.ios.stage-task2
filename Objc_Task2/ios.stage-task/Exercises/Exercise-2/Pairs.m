02051376e90a338ef0af168f30b699f910739444
Maksim Kirilko <maksimkirilko@gmail.com>
2021-05-15 18:34:23 +0300

    Fix task#4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -5,7 +5,7 @@
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
     int counter = 0;
     for (int i = 0; i < array.count; i++) {
-        for (int j = i; j < array.count; j++) {
+        for (int j = i + 1; j < array.count; j++) {
             if (array[j].intValue - array[i].intValue == number.intValue) {
                 counter += 1;
             }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,29 +3,26 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    NSMutableArray *oneDArray = [NSMutableArray new];
-    bool hasString = false;
-    bool hasNumber = false;
+    if (array == nil || array.count == 0) {
+        return @[];
+    }
+    NSMutableArray<NSNumber*> *numbers = [NSMutableArray new];
+    NSMutableArray<NSString*> *strings = [NSMutableArray new];
     for (int i = 0; i < array.count; i++) {
         if (![array[i] isKindOfClass:[NSArray class]]) {
             return @[];
         }
         for (int j = 0; j < array[i].count; j++) {
-            if (!hasString && ([array[i][j] isKindOfClass:[NSString class]])) {
-                hasString = true;
+            if (([array[i][j] isKindOfClass:[NSString class]])) {
+                [strings addObject:array[i][j]];
             }
-            if (!hasNumber && [array[i][j] isKindOfClass:[NSNumber class]]) {
-                hasNumber = true;
+            if ([array[i][j] isKindOfClass:[NSNumber class]]) {
+                [numbers addObject:array[i][j]];
             }
-            if (hasString && hasNumber) {
-                return @[];
-            }
-            [oneDArray addObject:(id)array[i][j]];
         }
     }
-    if (!hasString && hasNumber) {
-        return [oneDArray sortedArrayUsingComparator: ^(id obj1, id obj2) {
-         
+    if (strings.count == 0) {
+        return [numbers sortedArrayUsingComparator: ^(id obj1, id obj2) {
             if ([obj1 integerValue] > [obj2 integerValue]) {
                 return (NSComparisonResult)NSOrderedDescending;
             }
@@ -35,10 +32,23 @@
             return (NSComparisonResult)NSOrderedSame;
         }];
     }
-    if (hasString && !hasNumber) {
-        return [oneDArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    if (numbers.count == 0) {
+        return [strings sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
     }
-    return @[];
+    NSArray<NSNumber*> *sortedNumbers = [numbers sortedArrayUsingComparator: ^(id obj1, id obj2) {
+        if ([obj1 integerValue] > [obj2 integerValue]) {
+            return (NSComparisonResult)NSOrderedDescending;
+        }
+        if ([obj1 integerValue] < [obj2 integerValue]) {
+            return (NSComparisonResult)NSOrderedAscending;
+        }
+        return (NSComparisonResult)NSOrderedSame;
+    }];
+    
+    NSSortDescriptor *sortOrder = [NSSortDescriptor sortDescriptorWithKey: @"self" ascending: NO];
+    [strings sortUsingDescriptors:[NSArray arrayWithObject: sortOrder]];
+    
+    return [NSArray arrayWithObjects:sortedNumbers, strings, nil];
 }
 
 @end
